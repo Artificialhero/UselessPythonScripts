80ad5718c585d3510e35925007c31df37fe1dfb8
Etum <catsarebestthing@outlook.com>
2020-09-22 21:54:06 +0000

    Add files via upload

---
+++ autbook/mapIt.py
@@ -0,0 +1,14 @@
+import sys, webbrowser, pyperclip
+
+#Get address from clipboard or as argument
+#If no address is specified, it will use the content of the clipboard.
+#open webpage with Google maps to that address
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+    print(address)
+else:
+    address = pyperclip.paste()
+    print(address)
+    
+
+webbrowser.open('https://www.google.com/maps/place/'address)
